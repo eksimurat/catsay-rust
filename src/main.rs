1fdd47db3c9ae890e897b7b17d06c143f3fd3f2d
murikarox <eksi@kth.se>
2020-05-07 18:20:09 +0200

    add print_cat macro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+extern crate colored;
+extern crate structopt;
+
+use colored::*;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(default_value = "Meow!")]
+    /// What does the cat say?
+    message: String,
+
+    #[structopt(short = "d", long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
+
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    /// Load the cat figure from a specified file
+    catfile: Option<std::path::PathBuf>,
+}
+
+macro_rules! print_cat {
+    ($options:expr) => {
+        let eye = if $options.dead { "X" } else { "O" };
+        println!("{}", $options.message);
+
+        match $options.catfile {
+            Some(path) => {
+                let cat_template = std::fs::read_to_string(&path)
+                    .expect(&format!("could not read file {:?}", path));
+                let cat_picture = cat_template.replace("{eye}", eye);
+                println!("{}", &cat_picture);
+            }
+            None => {
+                println!(" \\");
+                println!("  \\");
+                println!("     /\\_/\\");
+                println!("    ( {eye} {eye} )", eye = eye.red().bold());
+                println!("    =( I )=")
+            }
+        }
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = Options::from_args();
+    print_cat!(options);
 }
